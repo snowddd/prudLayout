5cdd4ff1906928be2a2638fef2f13c99e16227af
snowd <snowmaos@gmail.com>
2020-06-08 09:37:56 +0800

    deploy

---
+++ json/bankCodeOptionList.js
@@ -0,0 +1,257 @@
+export default {
+  bankCodeOption: [
+    { BankCode: "004", BankName: "台灣銀行", text: "004 台灣銀行" },
+    { BankCode: "005", BankName: "土地銀行", text: "005 土地銀行" },
+    { BankCode: "006", BankName: "合作金庫銀行", text: "006 合作金庫銀行" },
+    { BankCode: "007", BankName: "第一銀行", text: "007 第一銀行" },
+    { BankCode: "008", BankName: "華南銀行", text: "008 華南銀行" },
+    { BankCode: "009", BankName: "彰化銀行", text: "009 彰化銀行" },
+    { BankCode: "011", BankName: "上海銀行", text: "011 上海銀行" },
+    { BankCode: "012", BankName: "台北富邦銀行", text: "012 台北富邦銀行" },
+    { BankCode: "013", BankName: "國泰世華銀行", text: "013 國泰世華銀行" },
+    { BankCode: "016", BankName: "高雄銀行", text: "016 高雄銀行" },
+    { BankCode: "017", BankName: "兆豐國際商銀", text: "017 兆豐國際商銀" },
+    { BankCode: "018", BankName: "全國農業金庫", text: "018 全國農業金庫" },
+    {
+      BankCode: "020",
+      BankName: "日商瑞穗實業銀行",
+      text: "020 日商瑞穗實業銀行"
+    },
+    { BankCode: "021", BankName: "花旗銀行", text: "021 花旗銀行" },
+    { BankCode: "022", BankName: "美國銀行", text: "022 美國銀行" },
+    { BankCode: "025", BankName: "菲律賓首都銀行", text: "025 菲律賓首都銀行" },
+    { BankCode: "039", BankName: "澳盛商業銀行", text: "039 澳盛商業銀行" },
+    {
+      BankCode: "040",
+      BankName: "中華開發工業銀行",
+      text: "040 中華開發工業銀行"
+    },
+    { BankCode: "048", BankName: "王道商業銀行", text: "048 王道商業銀行" },
+    {
+      BankCode: "050",
+      BankName: "臺灣中小企業銀行",
+      text: "050 臺灣中小企業銀行"
+    },
+    {
+      BankCode: "052",
+      BankName: "渣打國際商業銀行",
+      text: "052 渣打國際商業銀行"
+    },
+    { BankCode: "053", BankName: "台中商業銀行", text: "053 台中商業銀行" },
+    { BankCode: "054", BankName: "京城商業銀行", text: "054 京城商業銀行" },
+    { BankCode: "072", BankName: "德商德意志銀行", text: "072 德商德意志銀行" },
+    { BankCode: "075", BankName: "香港商東亞銀行", text: "075 香港商東亞銀行" },
+    {
+      BankCode: "081",
+      BankName: "香港上海匯豐銀行",
+      text: "081 香港上海匯豐銀行"
+    },
+    {
+      BankCode: "082",
+      BankName: "法商法國巴黎銀行",
+      text: "082 法商法國巴黎銀行"
+    },
+    {
+      BankCode: "086",
+      BankName: "法商東方匯理銀行",
+      text: "086 法商東方匯理銀行"
+    },
+    {
+      BankCode: "098",
+      BankName: "日商三菱東京日聯銀行",
+      text: "098 日商三菱東京日聯銀行"
+    },
+    {
+      BankCode: "099",
+      BankName: "比利時商比利時聯合銀行",
+      text: "099 比利時商比利時聯合銀行"
+    },
+    { BankCode: "101", BankName: "瑞興銀行", text: "101 瑞興銀行" },
+    { BankCode: "102", BankName: "華泰商業銀行", text: "102 華泰商業銀行" },
+    { BankCode: "103", BankName: "台灣新光商銀", text: "103 台灣新光商銀" },
+    {
+      BankCode: "104",
+      BankName: "台北市第五信用合作社",
+      text: "104 台北市第五信用合作社"
+    },
+    {
+      BankCode: "106",
+      BankName: "台北市第九信用合作社",
+      text: "106 台北市第九信用合作社"
+    },
+    { BankCode: "108", BankName: "陽信商業銀行", text: "108 陽信商業銀行" },
+    {
+      BankCode: "114",
+      BankName: "基隆第一信用合作社",
+      text: "114 基隆第一信用合作社"
+    },
+    {
+      BankCode: "115",
+      BankName: "基隆第二信用合作社",
+      text: "115 基隆第二信用合作社"
+    },
+    { BankCode: "118", BankName: "板信商業銀行", text: "118 板信商業銀行" },
+    {
+      BankCode: "119",
+      BankName: "淡水第一信用合作社",
+      text: "119 淡水第一信用合作社"
+    },
+    {
+      BankCode: "120",
+      BankName: "新北市淡水信用合作社",
+      text: "120 新北市淡水信用合作社"
+    },
+    { BankCode: "124", BankName: "宜蘭信用合作社", text: "124 宜蘭信用合作社" },
+    { BankCode: "127", BankName: "桃園信用合作社", text: "127 桃園信用合作社" },
+    {
+      BankCode: "130",
+      BankName: "新竹市第一信用合作社",
+      text: "130 新竹市第一信用合作社"
+    },
+    {
+      BankCode: "132",
+      BankName: "新竹市第三信用合作社",
+      text: "132 新竹市第三信用合作社"
+    },
+    {
+      BankCode: "146",
+      BankName: "台中市第二信用合作社",
+      text: "146 台中市第二信用合作社"
+    },
+    { BankCode: "147", BankName: "三信商業銀行", text: "147 三信商業銀行" },
+    {
+      BankCode: "158",
+      BankName: "彰化市第一信用合作社",
+      text: "158 彰化市第一信用合作社"
+    },
+    {
+      BankCode: "161",
+      BankName: "彰化市第五信用合作社",
+      text: "161 彰化市第五信用合作社"
+    },
+    {
+      BankCode: "162",
+      BankName: "彰化縣第六信用合作社",
+      text: "162 彰化縣第六信用合作社"
+    },
+    {
+      BankCode: "163",
+      BankName: "彰化市第十信用合作社",
+      text: "163 彰化市第十信用合作社"
+    },
+    {
+      BankCode: "165",
+      BankName: "彰化縣鹿港信用合作社",
+      text: "165 彰化縣鹿港信用合作社"
+    },
+    {
+      BankCode: "178",
+      BankName: "嘉義市第三信用合作社",
+      text: "178 嘉義市第三信用合作社"
+    },
+    {
+      BankCode: "179",
+      BankName: "嘉義市第四信用合作社",
+      text: "179 嘉義市第四信用合作社"
+    },
+    {
+      BankCode: "183",
+      BankName: "台南縣新營信用合作社",
+      text: "183 台南縣新營信用合作社"
+    },
+    {
+      BankCode: "188",
+      BankName: "台南市第三信用合作社",
+      text: "188 台南市第三信用合作社"
+    },
+    {
+      BankCode: "204",
+      BankName: "高雄市第三信用合作社",
+      text: "204 高雄市第三信用合作社"
+    },
+    {
+      BankCode: "215",
+      BankName: "花蓮第一信用合作社",
+      text: "215 花蓮第一信用合作社"
+    },
+    {
+      BankCode: "216",
+      BankName: "花蓮第二信用合作社",
+      text: "216 花蓮第二信用合作社"
+    },
+    {
+      BankCode: "222",
+      BankName: "澎湖縣第一信用合作社",
+      text: "222 澎湖縣第一信用合作社"
+    },
+    {
+      BankCode: "223",
+      BankName: "澎湖縣第二信用合作社",
+      text: "223 澎湖縣第二信用合作社"
+    },
+    {
+      BankCode: "224",
+      BankName: "金門縣信用合作社",
+      text: "224 金門縣信用合作社"
+    },
+    { BankCode: "700", BankName: "中華郵政", text: "700 中華郵政" },
+    { BankCode: "803", BankName: "聯邦銀行", text: "803 聯邦銀行" },
+    {
+      BankCode: "805",
+      BankName: "遠東國際商業銀行",
+      text: "805 遠東國際商業銀行"
+    },
+    { BankCode: "806", BankName: "元大商業銀行", text: "806 元大商業銀行" },
+    { BankCode: "807", BankName: "永豐商業銀行", text: "807 永豐商業銀行" },
+    { BankCode: "808", BankName: "玉山商業銀行", text: "808 玉山商業銀行" },
+    { BankCode: "809", BankName: "萬泰商業銀行", text: "809 萬泰商業銀行" },
+    { BankCode: "810", BankName: "星展銀行", text: "810 星展銀行" },
+    {
+      BankCode: "812",
+      BankName: "台北國際商業銀行",
+      text: "812 台北國際商業銀行"
+    },
+    { BankCode: "814", BankName: "大眾商業銀行", text: "814 大眾商業銀行" },
+    {
+      BankCode: "815",
+      BankName: "日盛國際商業銀行",
+      text: "815 日盛國際商業銀行"
+    },
+    { BankCode: "816", BankName: "安泰商業銀行", text: "816 安泰商業銀行" },
+    {
+      BankCode: "822",
+      BankName: "中國信託商業銀行",
+      text: "822 中國信託商業銀行"
+    }
+  ],
+  chooseRemitoptions: [
+    {
+      id: "001",
+      BankCode: "803",
+      BankAccounts: "009123456458",
+      BankName: "聯邦銀行",
+      text: "803 聯邦銀行 帳號：009******458"
+    },
+    {
+      id: "002",
+      BankCode: "810",
+      BankAccounts: "036123456201",
+      BankName: "星展銀行",
+      text: "810 星展銀行 帳號：036******201"
+    },
+    {
+      id: "003",
+      BankCode: "017",
+      BankAccounts: "019123456480",
+      BankName: "兆豐國際商銀",
+      text: "017 兆豐國際商銀 帳號：019******480"
+    },
+    {
+      id: "004",
+      BankCode: "808",
+      BankAccounts: "002123456012",
+      BankName: "玉山銀行",
+      text: "808 玉山銀行 帳號：002******012"
+    }
+  ]
+};
---
+++ precache-manifest.13cee35ce86d85a4cfcf1f21722b4557.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebfd0e80d8397d91aaf5",
+    "url": "/prudLayout/css/app.ae71b5f1.css"
+  },
+  {
+    "revision": "ebfd0e80d8397d91aaf5",
+    "url": "/prudLayout/js/app.8f3dfc00.js"
+  },
+  {
+    "revision": "ea2ce9f2bb837d8dcc57",
+    "url": "/prudLayout/css/chunk-0439df8a.576216db.css"
+  },
+  {
+    "revision": "ea2ce9f2bb837d8dcc57",
+    "url": "/prudLayout/js/chunk-0439df8a.a5c74779.js"
+  },
+  {
+    "revision": "5a7c061b503e422aa004",
+    "url": "/prudLayout/css/chunk-0b73582c.57fadce3.css"
+  },
+  {
+    "revision": "5a7c061b503e422aa004",
+    "url": "/prudLayout/js/chunk-0b73582c.5020f2a5.js"
+  },
+  {
+    "revision": "3e476ca2f3381177d5f5",
+    "url": "/prudLayout/css/chunk-1855852f.8df6f595.css"
+  },
+  {
+    "revision": "3e476ca2f3381177d5f5",
+    "url": "/prudLayout/js/chunk-1855852f.27a8b410.js"
+  },
+  {
+    "revision": "9e00f64b0c46cca80b97",
+    "url": "/prudLayout/css/chunk-2938db00.f2ad151b.css"
+  },
+  {
+    "revision": "9e00f64b0c46cca80b97",
+    "url": "/prudLayout/js/chunk-2938db00.c1b45ee3.js"
+  },
+  {
+    "revision": "a00aaf291ca2f56358c4",
+    "url": "/prudLayout/css/chunk-398ef0d8.d3468987.css"
+  },
+  {
+    "revision": "a00aaf291ca2f56358c4",
+    "url": "/prudLayout/js/chunk-398ef0d8.2f88af26.js"
+  },
+  {
+    "revision": "047a507ab962fa7b9e81",
+    "url": "/prudLayout/css/chunk-477a7750.5f34d92d.css"
+  },
+  {
+    "revision": "047a507ab962fa7b9e81",
+    "url": "/prudLayout/js/chunk-477a7750.9fca14b1.js"
+  },
+  {
+    "revision": "d6c2905eb09c91150ec4",
+    "url": "/prudLayout/css/chunk-50b2c4f3.414d8b40.css"
+  },
+  {
+    "revision": "d6c2905eb09c91150ec4",
+    "url": "/prudLayout/js/chunk-50b2c4f3.37b879bb.js"
+  },
+  {
+    "revision": "ff12adaabecc40bb29a9",
+    "url": "/prudLayout/css/chunk-5decd2a0.f70243b3.css"
+  },
+  {
+    "revision": "ff12adaabecc40bb29a9",
+    "url": "/prudLayout/js/chunk-5decd2a0.0fb9e359.js"
+  },
+  {
+    "revision": "5f80e1065ef9f595b350",
+    "url": "/prudLayout/css/chunk-5e6b1253.53904721.css"
+  },
+  {
+    "revision": "5f80e1065ef9f595b350",
+    "url": "/prudLayout/js/chunk-5e6b1253.60e474c9.js"
+  },
+  {
+    "revision": "e97eec9c6cd1f8ea5673",
+    "url": "/prudLayout/css/chunk-780d34dc.19787848.css"
+  },
+  {
+    "revision": "e97eec9c6cd1f8ea5673",
+    "url": "/prudLayout/js/chunk-780d34dc.7afef8fd.js"
+  },
+  {
+    "revision": "786b30da43d38d9faae0",
+    "url": "/prudLayout/css/chunk-a2acd340.e6412bc9.css"
+  },
+  {
+    "revision": "786b30da43d38d9faae0",
+    "url": "/prudLayout/js/chunk-a2acd340.763af6b6.js"
+  },
+  {
+    "revision": "9457cdf744d9f5036424",
+    "url": "/prudLayout/css/chunk-a2f21432.8ecb1786.css"
+  },
+  {
+    "revision": "9457cdf744d9f5036424",
+    "url": "/prudLayout/js/chunk-a2f21432.d0485b35.js"
+  },
+  {
+    "revision": "0d3e9c6a4a9ba3b2b67b",
+    "url": "/prudLayout/css/chunk-e37d805e.f7233d19.css"
+  },
+  {
+    "revision": "0d3e9c6a4a9ba3b2b67b",
+    "url": "/prudLayout/js/chunk-e37d805e.8b5d7288.js"
+  },
+  {
+    "revision": "eb775aef0476d90060a3",
+    "url": "/prudLayout/css/chunk-ef5489a2.ba5617ce.css"
+  },
+  {
+    "revision": "eb775aef0476d90060a3",
+    "url": "/prudLayout/js/chunk-ef5489a2.0109703c.js"
+  },
+  {
+    "revision": "a0f1b58419810a9e043f",
+    "url": "/prudLayout/css/chunk-vendors.4f0f47c4.css"
+  },
+  {
+    "revision": "a0f1b58419810a9e043f",
+    "url": "/prudLayout/js/chunk-vendors.7c4ff47b.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/prudLayout/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/prudLayout/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3dceb43e89be7859c58b6ea3d3d664eb",
+    "url": "/prudLayout/index.html"
+  },
+  {
+    "revision": "4d5a95cc9d2c865541ca59e25892e674",
+    "url": "/prudLayout/json/AllData.json"
+  },
+  {
+    "revision": "a1a94106948f2d7762aae56e0f0e80f9",
+    "url": "/prudLayout/json/bankCodeOptionList.js"
+  },
+  {
+    "revision": "35e2a2388c9d36a30b71d7a45dba08e5",
+    "url": "/prudLayout/img/bk_login.jpg"
+  },
+  {
+    "revision": "fb0c32261c7dacb8041a64c096bb2edb",
+    "url": "/prudLayout/img/login/logo.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/prudLayout/robots.txt"
+  }
+];
